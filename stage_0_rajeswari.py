195ef01814210203c0648df7b9ac7356d9beaaee
rajimedbio <69098187+rajimedbio@users.noreply.github.com>
2020-08-05 23:08:36 +0530

    Add files via upload

--- stage_0_rajeswari.py
+++ stage_0_rajeswari.py
@@ -1,1 +1,1 @@
-print("NAME: Rajeswari \nEMAIL: raji.medbio@gmail.com \nBIOSTACK: PUBLICATION IMAGE DESIGN \nSLACK USERNAME: @rajimedbio")
+print("NAME: Rajeswari \nEMAIL: raji.medbio@gmail.com \nBIOSTACK: PUBLICATION IMAGE DESIGN \nUSED LANGUAGE: Python \nSLACK USERNAME: @Rajeswari")
